6570dc488e826e241011dadf8b3a4d1e09d64ef0
media-code-now <mediacodenow@gmail.com>
2024-02-09 01:39:26 -0800

    main

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "random@example.com";
+        $recipient = "Arissa7000@gmail.com";
 
         // Set the email subject.
         $subject = "$message";
